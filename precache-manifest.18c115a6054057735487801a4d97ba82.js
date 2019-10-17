46034bdc8be3192957c26bc8a9c755fe535068ad
Sergey Pushkovskiy <push_s@mail.ua>
2019-10-17 22:19:20 +0300

    Updates

---
+++ precache-manifest.18c115a6054057735487801a4d97ba82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577ccaacaff13dbcaa3828851b7572ae",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "851697d9132edb229341",
+    "url": "/goit-react-hw-02-reader/static/css/main.e417f1ac.chunk.css"
+  },
+  {
+    "revision": "10a1434c87e493d4cdcf",
+    "url": "/goit-react-hw-02-reader/static/js/2.6c136cc4.chunk.js"
+  },
+  {
+    "revision": "851697d9132edb229341",
+    "url": "/goit-react-hw-02-reader/static/js/main.6c776cae.chunk.js"
+  },
+  {
+    "revision": "58b57449e5428d7bb638",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.4603fe05.js"
+  }
+]);
